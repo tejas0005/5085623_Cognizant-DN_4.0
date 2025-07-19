a1575f4f0cd4fe56d4e09112fe2871fce2cac51d
tejas0005 <147642356+tejas0005@users.noreply.github.com>
2025-07-19 21:28:07 +0530

    Add files via upload

---
+++ Week5/Creating Microservices for account and loan/lona/src/test/java/com/cognizant/lona/LonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.lona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
